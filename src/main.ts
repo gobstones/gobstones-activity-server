325620149176b14d46db936384b4c40ff51f07f9
Federico Aloi <federico.aloi@gmail.com>
2021-10-07 18:29:39 -0300

    Habilitado CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { EnvConfig } from './env-config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = app.get(EnvConfig);
   await app.listen(config.port);
 }
